037d69575d4c4b27feb7bff9544d892e0803a9de
traies <traies@itba.edu.ar>
2018-05-19 14:48:26 -0300

    Added Easylogging++ for logging

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include <openssl/md5.h>
 #include <openssl/evp.h>
 #include <cxxopts.hpp>
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
 
 int main() 
 {
     cxxopts::Options options("My program", "hello");
     bool compile = false;
 
+    LOG(INFO) << "HELLO WORLD!!!";
     options.add_options()
     ("f,file", "File", cxxopts::value<std::string>())
     ("c,compile", "Compile", cxxopts::value(compile));
